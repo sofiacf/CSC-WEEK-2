03d58146442115b65aa73dba1e35d34fd66467ad
hello7s <strugglemonkey@gmail.com>
2020-09-16 20:43:52 -0400

    Homework - Week 2
    Uploaded problems for week 3

---
+++ problem1.cpp
@@ -0,0 +1,9 @@
+// Text-printing program.
+#include <iostream> // enables program to output data to the screen
+
+// function main begins program execution
+int main {
+   std::cout << "Welcome to \nC++ape\n C++od!\n"; // display message
+
+   return 1; // indicate that program ended successfully
+} // end function main
---
+++ problem2.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // enables program to output data to the screen
+
+// function main begins program execution
+int main() {
+   cout << "Welcome\nto\n\nC++!\n";
+} // end function main
